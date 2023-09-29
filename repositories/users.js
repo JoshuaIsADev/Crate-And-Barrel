77ead87e903e68a3b42b27b9b05b4aaeaf24c5bd
JoshuaIsADev <JoshuaIsADev@gmail.com>
2023-09-28 22:30:45 -0700

    parsed getAll content using JSON.parse to get normal js object, refactored getAll function into one line, also made create method that takes in attributes email and password

--- repositories/users.js
+++ repositories/users.js
@@ -16,24 +16,37 @@ class UsersRepository {
 
   async getAll() {
     //Open the file called this.filename
-    const contents = await fs.promises.readFile(this.filename, {
-      encoding: 'utf8',
-    });
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8',
+      })
+    );
 
     //Read contents
-    console.log(contents);
+    // console.log(contents);
 
     //parse the contents
+    // const data = JSON.parse(contents);
 
     //return the parsed data
+    // return data;
+  }
+
+  async create(attrs) {
+    //{email:'r9r9ru23@yahoo.com', passwords: 'j3rrrr'}
+    const records = await this.getAll();
+    records.push(attrs);
+    //write the updated records array back to this.filename
+    await fs.promises.writeFile(this.filename, JSON.stringify(records));
   }
 }
 
 //TESTING CODE HELPER FUNCTION
 const test = async () => {
   const repo = new UsersRepository('users.json');
-
-  await repo.getAll();
+  await repo.create({ email: ' test@test.com', password: 'password' });
+  const users = await repo.getAll();
+  console.log(users);
 };
 
 test();
