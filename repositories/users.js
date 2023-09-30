b993ce5b83e9b5c358aa6742dd4e1e837c3bc2cc
JoshuaIsADev <JoshuaIsADev@gmail.com>
2023-09-29 21:19:31 -0700

    added radom ID generator, getOne by ID method, and delete method

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
   constructor(filename) {
@@ -33,20 +34,42 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId();
+
     //{email:'r9r9ru23@yahoo.com', passwords: 'j3rrrr'}
     const records = await this.getAll();
     records.push(attrs);
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
     //write the updated records array back to this.filename
-    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((record) => record.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(filteredRecords);
   }
 }
 
 //TESTING CODE HELPER FUNCTION
 const test = async () => {
   const repo = new UsersRepository('users.json');
-  await repo.create({ email: ' test@test.com', password: 'password' });
-  const users = await repo.getAll();
-  console.log(users);
+  await repo.delete('fcf6c04e');
 };
 
 test();
